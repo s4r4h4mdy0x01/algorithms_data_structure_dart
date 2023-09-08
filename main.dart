3f2fb123d55df748cdc852a4e67684c030006433
s4r4h4mdy0x01 <sara101hamdy@gmail.com>
2023-09-09 01:55:52 +0300

    smallest Equal Problem

--- main.dart
+++ main.dart
@@ -1,3 +1,7 @@
+import 'leetcode_problems/array/smallest_index_with_equal_value.dart';
+
 void main(List<String> args) {
-  print('sara');
+  var c = Solution();
+
+  print(c.smallestEqual([0, 1, 2]));
 }
